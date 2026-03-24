75219397675d8171b517e79f4f68a75078fbd747
Animesh Rudravaram <animesh.rudravaram@gmail.com>
2026-03-24 11:27:17 +0530

    Refactor: Code Structure and modularity Changes. *IMP-Might contain APK Build Fail. *

--- feature/topic/build.gradle.kts
+++ feature/topic/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.google.devtools.ksp)
 }
 
 android {
